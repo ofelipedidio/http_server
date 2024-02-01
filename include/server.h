fa0c00e34137fc15f7e8e19a346bf8ec97c62189
Felipe Didio <felipe.sdidio@gmail.com>
2024-02-01 08:48:16 -0300

    chore: Isolated server module

--- include/server.h
+++ include/server.h
@@ -1,12 +1,23 @@
 #ifndef SERVER
 #define SERVER
+
+#include <inttypes.h>
+#include <netinet/in.h>
 #include <stdint.h>
+
 #include "../include/bool.h"
+
+typedef struct {
+    uint16_t server_port;
+    struct sockaddr_in client;
+    int sockfd;
+} connection_t;
+
 
 /*
  * Starts a TCP server that listens on {port}.
  */
-bool_t start_server(uint16_t port);
+bool_t start_server( uint16_t in_port, int in_listen_count, int *out_listen_sockfd);
 
 void *start_http_server();
 
--- src/server.c
+++ src/server.c
@@ -19,10 +19,7 @@
 /*
  * Starts a TCP server that listens on {port}.
  */
-bool_t start_server(
-        uint16_t in_port,
-        int in_listen_count,
-        int *out_listen_sockfd) {
+bool_t start_server( uint16_t in_port, int in_listen_count, int *out_listen_sockfd) {
     struct sockaddr_in server_address;
 
     *out_listen_sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -43,50 +40,3 @@ bool_t start_server(
     return true;
 }
 
-
-
-void *start_http_server() {
-    int listen_sockfd;
-    int connection_sockfd;
-    socklen_t client_length;
-    struct sockaddr_in server_address;
-    struct sockaddr_in client_address;
-    uint16_t port = 8080;
-
-    if (!start_server(port, 15, &listen_sockfd)) {
-        fprintf(stderr, "ERROR: Could not listen on port %d", port);
-        perror(NULL);
-    }
-
-    fprintf(stdout, "[HTTP] Setup successful\n");
-
-    // Handle clients
-    {
-        client_length = sizeof(struct sockaddr_in);
-
-        while (1) {
-            // Accept connecting clients and break on 
-            connection_sockfd = accept(listen_sockfd, (struct sockaddr *) &client_address, &client_length);
-            if (connection_sockfd == -1) {
-                perror("ERROR: [DNS] Call to 'accept' failed");
-                // break;
-            } else {
-
-            allocate_to_thread((thread_input_t) {
-                    .server = server_address,
-                    .client = client_address,
-                    .sockfd = connection_sockfd,
-                    });
-            }
-        }
-    }
-
-    // Thread teardown
-    {
-        fprintf(stdout, "[HTTP] Closing\n");
-
-        close(listen_sockfd);
-        exit(EXIT_SUCCESS);
-        return NULL; 
-    }
-}
